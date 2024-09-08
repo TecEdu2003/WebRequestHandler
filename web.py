fdc190c5022e4922c2108a9f1653bd55e4adb4e8
TecEdu2003 <143043674+TecEdu2003@users.noreply.github.com>
2024-09-08 18:36:54 +0000

    Corrección para extraer la parte de la ruta antes del '?' y obtener el nombre del proyecto

--- web.py
+++ web.py
@@ -22,16 +22,27 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(self.get_response(host, user_agent, path).encode("utf-8"))
 
     def get_response(self,host, user_agent, path):
-        return f"""
-    <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
-    <h1>Ruta Solicitada: {path}</h1>
-    <p>Host: {host}</p>
-    <p>User-Agent: {user_agent}</p>
-"""
+        query = self.query_data()
+        # Extraer la parte antes del '?'
+        path_without_query = path.split('?')[0]
+        
+        # Extraer la parte después del último '/'
+        if path_without_query.startswith("/proyecto/"):
+            proyecto = path_without_query.split('/')[-1]
+            # Obtener el parámetro 'autor' de la query string
+            autor = query.get('autor', 'Desconocido')
+            return f"<h1>Proyecto: {proyecto} Autor: {autor}</h1>"
+        else:
+            return f"""
+        <h1> Hola Web </h1>
+        <p> URL Parse Result : {self.url()}         </p>
+        <p> Path Original: {self.path}         </p>
+        <p> Headers: {self.headers}      </p>
+        <p> Query: {self.query_data()}   </p>
+        <h1>Ruta Solicitada: {path}</h1>
+        <p>Host: {host}</p>
+        <p>User-Agent: {user_agent}</p>
+        """
 
 
 if __name__ == "__main__":
